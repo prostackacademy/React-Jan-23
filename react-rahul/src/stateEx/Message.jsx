a1cb3ab226a00a2664c0c0c0652d197ee0edbe15
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-01-24 12:42:31 +0530

    okie

--- react-rahul/src/stateEx/Message.jsx
+++ react-rahul/src/stateEx/Message.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
 
 class Message extends Component {
-    msg = "Hello"
+    state = {
+        msg: "Hello..."
+    }
     gmHandler = () => {
-        console.log("inside handler")
-        this.msg = "Good Morning Rahul"
-        console.log(this.msg)
-        this.forceUpdate()
+        this.setState({
+            msg: "Good Monring Rahul"
+        })
+    }
+    gaHandler = () => {
+        this.setState({
+            msg: "Good Afternoon Rahul"
+        })
+    }
+    gnHandler = () => {
+        this.setState({
+            msg: "Good Night Rahul"
+        })
     }
     render() {
+        console.log("render method -execting")
         return (
             <div>
-                <h1>Message value:{this.msg}</h1>
+                <h1>Message:{this.state.msg}</h1>
                 <button onClick={this.gmHandler}>GM</button>
+                <button onClick={this.gaHandler}>GA</button>
+                <button onClick={this.gnHandler}>GN</button>
             </div>
         )
     }
