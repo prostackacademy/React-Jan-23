afa374cab6a0775a0dcf6da3d45096900097ffdb
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-01-28 13:01:12 +0530

    okie

--- react-rahul/src/App.js
+++ react-rahul/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import Employee from './Employee/Employee'
+import Login from './FormHandling/Login'
+import User from './AxiosEx/User'
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Employee />
+      <User />
     </div>
   )
 }
