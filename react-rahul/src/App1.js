c3a5c5e91a0774bbce82b74520cbf58a0a8ce7e8
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-01-29 16:35:54 +0530

    okie

--- react-rahul/src/App1.js
+++ react-rahul/src/App1.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 import Navbar from './Navbar/Navbar'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
-import UserApp from './UserApp/UserApp'
+import About from './components/About'
+import Services from './components/Services'
+import Product from './components/Product'
+import Contact from './components/Contact'
 
 export class App extends Component {
   render() {
@@ -12,8 +15,8 @@ export class App extends Component {
           <Navbar />
           <Switch>
             <Route path="/home" component={Home} />
-            <Route path="/userApp" component={UserApp} />
-
+            <Route path="/about" component={About} />
+            <Route path="/services" component={Services} />
           </Switch>
         </Router>
       </div>
