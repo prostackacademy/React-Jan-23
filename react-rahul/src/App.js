97c819b696dc02ab2ea9a39e5ac2e2f96ebbb7df
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-02-02 12:21:50 +0530

    okie

--- react-rahul/src/App.js
+++ react-rahul/src/App.js
@@ -1,28 +1,16 @@
 import React from 'react'
-import Home from './components/Home'
-import User from './components/User'
-import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Message from './Message/Message'
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 const App = () => {
   return (
     <div>
-      <Router>
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-          <Link to="#" className="navbar-brand">Life Cycle Methods</Link>
-          <div className="ml-auto">
-            <ul className="navbar-nav">
-              <li className="nav-list"><Link className="nav-link" to="/home">Home</Link></li>
-              <li className="nav-list"><Link className="nav-link" to="/user">User</Link></li>
-
-            </ul>
-          </div>
-        </nav>
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/user" component={User} />
-
-        </Switch>
-      </Router>
-
+      <Provider store={store}>
+        <h1>App Component</h1>
+        <hr />
+        <h1>Message: Hello</h1>
+        <Message />
+      </Provider>
     </div>
   )
 }
